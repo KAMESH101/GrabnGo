07aa2dd120e77606d25072fce5652b333d24de76
S.Kamesh <kameshrich005@gmail.com>
2026-05-12 17:44:49 +0530

    chore: remove unnecessary files (error logs, replit config, old zips, redundant docs)

--- server/diag/check.js
+++
@@ -1,8 +0,0 @@
-try {
-    require('../node_modules/@vladmandic/face-api/dist/face-api.node-wasm.js');
-    console.log('OK');
-} catch (e) {
-    const fs = require('fs');
-    fs.writeFileSync('err_full.txt', 'MESSAGE:\n' + e.message + '\n\nSTACK:\n' + e.stack, 'utf8');
-    console.log('Error written to err_full.txt');
-}
